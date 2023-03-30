851b848380404037888782d3c9e36b180e6f1023
jshandilya <jahan01shandilya@gmail.com>
2023-03-30 17:01:24 -0400

    Implemented save state

--- EP-491-Saturation/Source/PluginProcessor.cpp
+++ EP-491-Saturation/Source/PluginProcessor.cpp
@@ -233,15 +233,18 @@ juce::AudioProcessorEditor* EP491SaturationAudioProcessor::createEditor()
 //==============================================================================
 void EP491SaturationAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    auto state = apvts.copyState();
+    std::unique_ptr<juce::XmlElement> xml (state.createXml());
+    copyXmlToBinary (*xml, destData);
 }
 
 void EP491SaturationAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+    
+    if (xmlState.get() != nullptr)
+        if (xmlState->hasTagName (apvts.state.getType()))
+            apvts.replaceState (juce::ValueTree::fromXml (*xmlState));
 }
 
 //==============================================================================
